a2a46bbf14e76ea0629ae4745e604c2abc6bc3d2
maeriss <71259448+maeriss@users.noreply.github.com>
2021-02-01 11:55:41 +0100

    Update BIP32.py

--- BIP32.py
+++ BIP32.py
@@ -1,21 +1,25 @@
 import hashlib
-import pbkdf2
 
-
+#trouver la root seed
 def keyStretching(mnemonic, passphrase = ""):
     mnemonic = mnemonic.encode('utf-8')
+    #salt composé du mot constant "mnemonic" et d'une phrase secrete optionnelle
     salt = "mnemonic" + passphrase
     salt = salt.encode('utf-8')
+    #dérivation de clé avec pbkdf2 sur 2048 itérations avec sha512
     key = hashlib.pbkdf2_hmac('sha512', mnemonic, salt, 2048).hex()
     return key
 
 
 
 if __name__ == "__main__":
+    #demande à l'utilisateur son mnémonique et une phrase secrète
     mnemonic = input("Veuillez saisir votre mnémonique : ")
     passphrase = input("(OPTIONNEL) Votre phrase secrète : ")
     key = keyStretching(mnemonic, passphrase)
+    #hachage de la root seed avec sha512
     hash_key = hashlib.sha512(key.encode()).hexdigest()
+    #séparation en 2 des 512 bits
     n = int(len(hash_key)/2)
     master_private_key = hash_key[0:n]
     master_chain_code = hash_key[n:]
